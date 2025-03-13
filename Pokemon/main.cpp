9638bef956fb8c6205e9ed2941fd3f6b79cf9a51
AlabasterRig <utkrist.j@gmail.com>
2025-03-13 18:44:09 +0530

    First Commit

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+using namespace std;
 
 int main() {
+
+    string PlayerName;
+	cout << "Enter your name: ";
+	cin >> PlayerName;
+
+	cout << "Great Start " << PlayerName << ", looks like you have understood the main() function properly now!" << endl;
 
     return 0;
 }
